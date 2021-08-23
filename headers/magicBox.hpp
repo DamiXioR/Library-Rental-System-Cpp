d988a2cff39c74f396a1de2d5e192c46bcd31264
Damixiorek <damixiorek@gmail.com>
2021-08-23 18:06:10 +0200

    refactorized magic box

--- headers/magicBox.hpp
+++ headers/magicBox.hpp
@@ -1,7 +1,6 @@
 #ifndef MAGICBOX
 #define MAGICBOX
 
-#include <limits>
 #include <random>
 #include <set>
 
@@ -9,26 +8,11 @@ class MagicBox {
 private:
 	std::set<unsigned long long> idBox_{};
 public:
-	std::size_t getIdBoxSize() const {
+	auto getIdBoxSize() const -> std::size_t {
 		return idBox_.size();
 	}
 
-	std::size_t drawId() {
-		unsigned long long minId {1};
-		unsigned long long maxId {ULLONG_MAX};
-		std::random_device rd;
-		std::uniform_int_distribution<unsigned long long> dist (minId, maxId);
-		bool isUnique{false};
-		unsigned drawedValue{0};
-		while(!isUnique){
-			auto insertedIt = idBox_.insert(dist(rd));
-			if(insertedIt.second == true) {
-				isUnique = true;
-				drawedValue = *insertedIt.first;
-			}
-		}
-		return drawedValue;
-	}
+	auto drawId() -> unsigned long long ;
 };
 
 #endif
--- src/magicBox.cpp
+++ src/magicBox.cpp
@@ -1,1 +1,20 @@
-#include "book.hpp"
+#include "magicBox.hpp"
+
+#include <climits>
+
+auto MagicBox::drawId() -> unsigned long long {
+	unsigned long long minId {1};
+	unsigned long long maxId {ULLONG_MAX};
+	std::random_device rd;
+	std::uniform_int_distribution<unsigned long long> dist (minId, maxId);
+	bool isUnique{false};
+	unsigned drawedValue{0};
+	while(!isUnique){
+		auto insertedIt = idBox_.insert(dist(rd));
+		if(insertedIt.second == true) {
+			isUnique = true;
+			drawedValue = *insertedIt.first;
+		}
+	}
+	return drawedValue;
+}
