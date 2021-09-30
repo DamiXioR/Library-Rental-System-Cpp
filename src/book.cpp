1e12f97654f56967984cbb231a9ee23aabe11cba
Damixiorek <damixiorek@gmail.com>
2021-09-30 08:41:45 +0200

    refactorize Book class with using directly variables no getters inside class

--- src/book.cpp
+++ src/book.cpp
@@ -14,6 +14,6 @@ Book::Book( std::string author,
 
  auto Book::getStringStreamOfBooksData() const -> std::stringstream {
 	std::stringstream ss;
-	ss << "Title: " << getTitle() << " Kind: " << getKind() << " Date: " << getDate() << " ID: " << getId();
+	ss << "Title: " << title_ << " Kind: " << kind_ << " Date: " << date_ << " ID: " << uniqueId_;
 	return ss;
 }
